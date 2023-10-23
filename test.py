a23c3fc1dfea9e09974bd74fe3c8382af03212b4
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-10-23 23:51:46 +0200

    start off

---
+++ test.py
@@ -0,0 +1,43 @@
+# def school_age_calculator(age, name):
+#     if age < 4:
+#         print("Enjoy your time.", name, "is only", age)
+#     elif age == 5:
+#         print("Enjoy kindergarten,", name)
+#     else:
+#         print("they grow up so fast!")
+
+# school_age_calculator(3, "Thomas")
+
+# def add_ten_to_age(age):
+#     new_age = age + 10
+#     return new_age
+
+# How_Old_Will_I_Be = add_ten_to_age(3)
+# print(How_Old_Will_I_Be)
+
+#while
+# x=0
+# while (x<5):
+#     print(x)
+#     x=x+1
+
+# #For
+# for x in range(5,10):
+#     print(x)
+# days=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+# # for d in days:
+# #     if(d=="Thu"):break
+# #     print(d)
+
+# # for d in days:
+# #     if(d=="Thu"):continue
+# #     print(d)
+
+# import math
+# print("pi is", math.pi)
+
+namn = 'Ida'
+print ('Hej', namn)
+
+print(6/3)
