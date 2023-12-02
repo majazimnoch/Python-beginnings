4d537982dbc3502450d7299f783735bd19039309
majazimnoch <majamzimnoch@gmail.com>
2023-12-02 11:55:31 +0100

    basic tasks with while

---
+++ inlamning3.py
@@ -0,0 +1,20 @@
+# x = 0
+# while x < 10:
+#     print(x)
+#     x += 1
+
+# y = 5
+# while y != 0:
+#     print(y)
+#     y -= 1
+
+
+# z = 10
+# while z != 0:
+#     print(z)
+
+color = input('Gissa vilken farg jag tanker pa: ')
+
+while color != 'gul':
+    color = input('fel, gissa igen: ')
+print('du gissade ratt')
