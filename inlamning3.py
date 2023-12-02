768709c0813d6cc7b151f91a3b5bca046df1e923
majazimnoch <majamzimnoch@gmail.com>
2023-12-02 14:14:21 +0100

    continue and while and break

--- inlamning3.py
+++ inlamning3.py
@@ -13,8 +13,34 @@
 # while z != 0:
 #     print(z)
 
-color = input('Gissa vilken farg jag tanker pa: ')
+# color = input('Gissa vilken farg jag tanker pa: ')
 
-while color != 'gul':
-    color = input('fel, gissa igen: ')
-print('du gissade ratt')
+# while color != 'gul':
+#     color = input('fel, gissa igen: ')
+# print('du gissade ratt')
+
+# summa = 0
+# k = 1
+
+# while k <= 5:
+#     summa += k
+#     k += 1
+
+# print('Resultat: ', summa)
+
+print(' ::: BILRESAN ::: ')
+while True:
+    print('My fragar: Ar vi framme snart?')
+    svar = input('Du: ')
+    if svar == 'ja':
+        break
+    elif svar == 'nej':
+        continue
+    elif 'glass' in svar:
+        print('my: Horde jag glass?')
+        svar = input('du: ')
+        if svar == 'ja':
+            print('My: jag vill ha glass genast')
+            continue
+    print('jag ar kissnodig!')
+print('My: Vilken tur')
