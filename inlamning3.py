29cb4675850014c5225f5fcfc4801a640b586ee0
majazimnoch <majamzimnoch@gmail.com>
2023-12-03 21:29:45 +0100

    tasks wth while

--- inlamning3.py
+++ inlamning3.py
@@ -28,19 +28,44 @@
 
 # print('Resultat: ', summa)
 
-print(' ::: BILRESAN ::: ')
-while True:
-    print('My fragar: Ar vi framme snart?')
-    svar = input('Du: ')
-    if svar == 'ja':
-        break
-    elif svar == 'nej':
-        continue
-    elif 'glass' in svar:
-        print('my: Horde jag glass?')
-        svar = input('du: ')
-        if svar == 'ja':
-            print('My: jag vill ha glass genast')
-            continue
-    print('jag ar kissnodig!')
-print('My: Vilken tur')
+# print(' ::: BILRESAN ::: ')
+# while True:
+#     print('My fragar: Ar vi framme snart?')
+#     svar = input('Du: ')
+#     if svar == 'ja':
+#         break
+#     elif svar == 'nej':
+#         continue
+#     elif 'glass' in svar:
+#         print('my: Horde jag glass?')
+#         svar = input('du: ')
+#         if svar == 'ja':
+#             print('My: jag vill ha glass genast')
+#             continue
+#     print('jag ar kissnodig!')
+# print('My: Vilken tur')
+
+# for i in range(10):
+#     print(i)
+
+# for hej in range(10):
+#     print(hej)
+
+# i = 5
+# while i < 15:
+#     print(i)
+#     i += 2
+# for i in range (5,10):
+#      print(i)
+
+tal = int(input("Type a number: "))
+while tal < 100:
+    print(tal)
+    tal *= 1.5
+
+# for i in range (25, 20, -2):
+#     print(i)
+
+# for x in range(5):
+#     print('#', end = '')
+
