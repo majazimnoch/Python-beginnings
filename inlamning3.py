9cbe423254831341d4979cd2fd573a385b090613
majazimnoch <majamzimnoch@gmail.com>
2023-12-07 11:02:15 +0100

    done

--- inlamning3.py
+++ inlamning3.py
@@ -1,4 +1,35 @@
-# Övning 1
+# Övning 1 och 2 och 3 och 4
+
+import random
+while True:
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    dice_sum = dice1 + dice2
+
+    number_tries = int(input("Skriv hur många attempts vill du ha: "))
+    guess_number = 0
+
+    for i in range(number_tries):
+        guess = int(input("Gissa summan av tärningarna (2-12): "))
+        guess_number += 1
+
+        if guess == dice_sum:
+            print(f'Grattis! Du fick rätt på summan som var {dice_sum} och du försökt {guess_number} gånger')
+            break
+        elif guess < dice_sum:
+            print("För låg. Försök igen")
+        else:
+            print("För hög. Försök igen")
+    print(f'Otur! Du hittade inte summan som var {dice_sum} och du försökt {guess_number} gånger.')
+
+    play_again = input("Önskar du att spela igen? Skriv ja eller nej: ")
+
+    if play_again != 'ja':
+        break
+
+    print("\nNytt spel börjar...\n")
+
+    
 
 # import random
 
@@ -21,32 +52,32 @@
 # print(f'Otur! Du hittade inte summan som var {dice_sum} och du försökt {guess_number} gånger.')
 
 
-import random
-while True:
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
-    dice_sum = dice1 + dice2
-    guess_number = 0
+# import random
+# while True:
+#     dice1 = random.randint(1,6)
+#     dice2 = random.randint(1,6)
+#     dice_sum = dice1 + dice2
+#     guess_number = 0
 
-    for i in range(3):
-        guess = int(input("Gissa summan av tärningarna (2-12): "))
-        guess_number += 1
+#     for i in range(3):
+#         guess = int(input("Gissa summan av tärningarna (2-12): "))
+#         guess_number += 1
 
-        if guess == dice_sum:
-            print(f'Grattis! Du fick rätt på summan som var {dice_sum} och du försökt {guess_number} gånger')
-            break
-        elif guess < dice_sum:
-            print("För låg. Försök igen")
-        else:
-            print("För hög. Försök igen")
-    print(f'Otur! Du hittade inte summan som var {dice_sum} och du försökt {guess_number} gånger.')
+#         if guess == dice_sum:
+#             print(f'Grattis! Du fick rätt på summan som var {dice_sum} och du försökt {guess_number} gånger')
+#             break
+#         elif guess < dice_sum:
+#             print("För låg. Försök igen")
+#         else:
+#             print("För hög. Försök igen")
+#     print(f'Otur! Du hittade inte summan som var {dice_sum} och du försökt {guess_number} gånger.')
 
-    play_again = input("Önskar du att spela igen? Skriv ja eller nej: ")
+#     play_again = input("Önskar du att spela igen? Skriv ja eller nej: ")
 
-    if play_again != 'ja':
-        break
+#     if play_again != 'ja':
+#         break
 
-    print("\nNytt spel börjar...\n")
+#     print("\nNytt spel börjar...\n")
 
 
 
