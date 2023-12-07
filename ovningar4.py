5d132a852a8aea450d03e2fd8a929ca61f76a45d
majazimnoch <majamzimnoch@gmail.com>
2023-12-07 21:10:14 +0100

    new tasks

---
+++ ovningar4.py
@@ -0,0 +1,24 @@
+# print('1\tJeh')
+# print('1hej\rJeh')
+
+# a = "Hello world"
+# i = a.startswith('Hello')
+
+# print(i)
+
+
+# a = "Hello world"
+# i = a.islower()
+
+# print(i)
+
+# a = "Hello world"
+# i = a.strip()
+
+# print(i)
+
+a = "Hello world"
+i = a.replace("Hello", "Good day")
+
+print(i)
+
