eddeebcdb2e43963d11b61945315ac16fe4f7aed
majazimnoch <majamzimnoch@gmail.com>
2023-12-17 22:33:46 +0100

    last homework done

--- inlamning4.py
+++ inlamning4.py
@@ -1,16 +1,38 @@
 # # 1
-# def skrivOrd(ordet):
-#     for i in range(len(ordet)):
-#         print(ordet)
-
-# valtOrd = input("Write a word: ")
-# skrivOrd(valtOrd)
-
-# 2
-
 def skrivOrd(ordet):
-    for i in range(1, len(ordet) + 1):
-        print(f'{i}. {ordet[i-1]}')
+    for i in range(len(ordet)):
+        print(ordet)
 
 valtOrd = input("Write a word: ")
 skrivOrd(valtOrd)
+
+# # 2
+
+def skrivOrd(ordet):
+    for i in range(1, len(ordet) + 1):
+        print(f'{i}. {ordet}')
+
+valtOrd = input("Write a word: ")
+
+skrivOrd(valtOrd)
+
+# # 3 and 4
+
+def FindGreatestNumber(a, b, c):
+    GreatestNumber = a
+
+    if b > GreatestNumber:
+        GreatestNumber = b
+
+    if c > GreatestNumber:
+        GreatestNumber = c
+
+    return GreatestNumber
+
+a = 7888
+b = 89
+c = 9
+
+Greatest = FindGreatestNumber(a, b, c)
+
+print("The greater number is: ", Greatest)
